94587bb3210b3d4b9ce288a1d81e8048b7c2d3f1
Michelle Yu <y.michelle03@gmail.com>
2019-03-25 22:38:40 -0400

    changed to swift 5 and updated Xcode 10.2.

--- Parstagram/FeedViewController.swift
+++ Parstagram/FeedViewController.swift
@@ -6,20 +6,65 @@
 //
 
 import UIKit
+import Parse
+import AlamofireImage
 
-class FeedViewController: UIViewController {
-
+class FeedViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    var posts = [PFObject]()
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        tableView.delegate = self;
+        tableView.dataSource = self;
     }
     
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated); // refresh
+        // parse:
+        let query = PFQuery(className: "Posts");
+        query.includeKey("author");
+        query.limit = 20;
+        
+        query.findObjectsInBackground{ (posts, Error) in
+            if posts != nil {
+                self.posts = posts!;
+                self.tableView.reloadData();
+            }
+        }
+    }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return posts.count;
+    }
+    
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell") as! PostTableViewCell
+       
+        let post = posts[indexPath.row]
+        let user = post["author"] as! PFUser
+        
+        cell.usernameLabel.text = user.username
+        cell.captionLabel.text = post["caption"] as? String
+        
+        let imageFile = post["image"] as! PFFileObject
+        let urlString = imageFile.url!
+        let url = URL(string: urlString)!
+        
+        cell.photoView.af_setImage(withURL: url)
+
+        return cell;
+    }
+    
+  
+
 
     /*
     // MARK: - Navigation
