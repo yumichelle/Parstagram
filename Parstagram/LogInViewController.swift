f7ba89e93d00d4e05d4809cc8addeefa66ed6837
Michelle Yu <y.michelle03@gmail.com>
2019-03-30 15:00:59 -0400

    adding little features to camera view controller

--- Parstagram/LogInViewController.swift
+++ Parstagram/LogInViewController.swift
@@ -42,8 +42,8 @@ class LoginViewController: UIViewController {
                 self.performSegue(withIdentifier: "loginSegue", sender: nil)
                 
             } else {
-                print("error:  \(error!.localizedDescription)");
-                
+                print("error:  \(String(describing: error?.localizedDescription))");
+
             }
         }
 
@@ -61,7 +61,7 @@ class LoginViewController: UIViewController {
                 print("sign up success")
             }
             else {
-                print("error:  \(error!.localizedDescription)");
+                print("error:  \(String(describing: error?.localizedDescription))");
             }
         }
     }
